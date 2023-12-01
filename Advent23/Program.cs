e8ac4a0597fdee779cd2863f980e5e447d6589fd
RonHaland <ronnyhaland@gmail.com>
2023-12-01 10:21:18 +0100

    🔧 Setup

---
+++ Advent23/Program.cs
@@ -0,0 +1,22 @@
+﻿using Advent23.Solutions;
+using System.Reflection;
+
+Console.WriteLine("Enter day to run or press enter to run latest");
+
+var days = Assembly.GetExecutingAssembly().DefinedTypes.Where(t => t.IsAssignableTo(typeof(BaseDay)) && t.Name != "BaseDay").Select(s => (BaseDay)s.GetConstructor(Array.Empty<Type>()).Invoke(new object[0]));
+
+days = days.OrderBy(n => n.Number);
+
+Console.WriteLine($"Valid days: {string.Join(", ", days.Select(d => d.Number))}");
+
+var res = Console.ReadLine();
+
+
+if (int.TryParse(res, out var number) && days.Any(d => d.Number == number))
+{
+    days.First(d => d.Number == number).Run();
+}
+else
+{
+    days.Last().Run();
+}
---
+++ Advent23/Solutions/BaseDay.cs
@@ -0,0 +1,16 @@
+﻿namespace Advent23.Solutions;
+
+public abstract class BaseDay
+{
+    public int Number { get; set; }
+    public string Input { get; set; }
+    public string[] InputLines { get; set; }
+    public BaseDay(int number)
+    {
+        Input = File.ReadAllText($"./Inputs/{number}.txt");
+        InputLines = Input.Split(Environment.NewLine);
+        Number = number;
+    }
+
+    public abstract void Run();
+}
